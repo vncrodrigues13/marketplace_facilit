3ec9faae58e553f8c3e56a394058c86f3ccfc625
vncrodrigues13 <vinicius@facilit.com.br>
2021-12-01 08:13:15 -0300

    "Update"

--- src/main/java/com/marketplace/facilit/controllers/ProductController.java
+++ src/main/java/com/marketplace/facilit/controllers/ProductController.java
@@ -1,14 +1,49 @@
 package com.marketplace.facilit.controllers;
 
 import java.util.List;
-import com.marketplace.facilit.dto.ProductDTO;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.marketplace.facilit.dto.ProductDTO;
+import com.marketplace.facilit.exceptions.ProductNotFoundException;
+import com.marketplace.facilit.forms.ProductForm;
+import com.marketplace.facilit.impl.ProductImpl;
+import com.marketplace.facilit.services.ProductService;
+
+@RestController()
+@RequestMapping(value="/product")
 public class ProductController {
 
-	
-	public List<ProductDTO> listProducts() {
-		
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping()
+	public List<ProductImpl> findAllProducts() {
+		return productService.findAllProducts();
 	}
+	
+	@GetMapping("/{id}")
+	public ProductDTO findProductById(@PathVariable Long id) throws ProductNotFoundException {
+		ProductImpl product = productService.findProduct(id);
+		
+		return new ProductDTO(product);
+	}
+	
+	
+//	@PostMapping()
+//	public ProductDTO saveProduct(@RequestBody ProductForm productForm) {
+//		
+//		ProductImpl product = productForm.saveProduct(productRepository);
+//		
+//		return new ProductDTO(product);
+//	}
 }
--- src/main/java/com/marketplace/facilit/dto/ProductDTO.java
+++ src/main/java/com/marketplace/facilit/dto/ProductDTO.java
@@ -1,40 +1,52 @@
 package com.marketplace.facilit.dto;
 
+import com.marketplace.facilit.impl.ProductImpl;
 import com.marketplace.facilit.models.Product;
 
 public class ProductDTO {
 	
+	
 	private String name;
 	private float price;
+	private long id;
 	
 	
-	public ProductDTO(String name, float price) {
-		super();
-		this.name = name;
-		this.price = price;
-	}
-	
-	
-	public ProductDTO(Product product) {
-		super();
+	public ProductDTO(ProductImpl product) {
+		this.id = product.getId();
 		this.name = product.getName();
 		this.price = product.getPrice();
 	}
-	
+
+
 	public String getName() {
 		return name;
 	}
-	
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+
 	public float getPrice() {
 		return price;
 	}
-	
+
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
 	
 }
